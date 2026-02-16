47238b2eb9d245f020739e1d93fb81b4589a3e71
Tj-Bots <tjbots123@gmail.com>
2026-02-16 08:22:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -17,4 +17,4 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
 UPDATE_CHANNEL = "amoviesnww1o"
 REQUEST_GROUP = "https://t.me/+t0Bi1P95xps1Mzk0"
 
-PHOTO_URL = "https://telegra.ph/file/ba7f0d4e9bead181a4027-5392cd54196b2d10a5.jpg"
+PHOTO_URL = "https://telegra.ph/file/3fa89085f20e4e7e6e38b-c522eb8a573d3961e6.jpg"
